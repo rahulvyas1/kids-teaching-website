645b144352acc18b38ab3068030acac274cf4e00
somil <somildhane@gmail.com>
2020-03-22 18:34:32 +0530

    upcoming courses ui

--- src/Assets/style.js
+++ src/Assets/style.js
@@ -41,3 +41,39 @@ export const PaddingWrapper = styled.div`
         align-items:center;
     }
 `;
+export const Sqwrapper = styled.div`
+    background-image: linear-gradient(to bottom, #eef9ff -10%, #ade1ff);
+    position:relative;
+    top: -30px;
+&:before {
+    content: '';
+    background-image: linear-gradient(to top,#eef9ff -10%,#ade1ff);
+    position: absolute;
+    height: 96px;
+    width: 100%;
+    left: 0;
+    right: 0;
+    top: -96px;
+    clip-path: ellipse(53% 58% at 50% 103%);
+}
+    .card-title {
+        font-size:17px;
+        color: #1f2a55;
+        text-align:left;
+
+    }
+    .card-body {
+        text-align:left;
+        .sm-txt {
+            font-size:12px;
+            color:#777b8c;
+            font-weight: 500;
+        }
+        .standard {
+            font-size:14px;
+            color:#1f2a55;
+            font-weight: 600;
+            padding: 0px 17px;
+        }
+    }
+`;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,51 @@
 import React from 'react'
 import BannerComponent from '../components/BannerComponent';
+import { Container, Navbar, Nav, Button, Row, Col, Card } from 'react-bootstrap'
+import logo from '../Assets/logo.svg'
+import Leaderboard from '../Assets/leaderboard.svg'
+import CampCoin from '../Assets/campcoins.svg'
+import BannerImg from '../Assets/header-photo.png'
+import AndroidThumb from '../Assets/and.jpg'
+import Grades from '../Assets/grades.svg'
+import Req from '../Assets/req.svg'
+import { NavRight, BannerWrapper, PaddingWrapper, Sqwrapper } from '../Assets/style'
 
 
 const Home = () => {
     return (
+        <>
         <BannerComponent />
+        <Sqwrapper>
+            <Container>
+                <Row>
+                    <Col expand="md">
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={AndroidThumb} />
+                            <Card.Body>
+                                <Card.Title>Android App Development Level 1</Card.Title>
+                                <Card.Text>
+                                <Row>
+                                    <Col expand="md">
+                                        <img src={Grades} /> <span className="sm-txt">GRADES</span> <br/>
+                                        <span className="standard">4 - 8</span>
+                                    </Col>
+                                    <Col expand="md" className="text-center">
+                                        <img src={Req} /> <span className="sm-txt">PRE-REQ</span> <br/>
+                                        <span className="standard">Yes</span>
+                                    </Col>
+                                </Row>
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col expand="md"></Col>
+                    <Col expand="md"></Col>
+                </Row>
+            </Container>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        </Sqwrapper>
+      </>
     );
 }
 
