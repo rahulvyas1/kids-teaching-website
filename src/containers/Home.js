3eac9fa6652988760f5d4eed7160ed53242495e9
Pravin <isppal02@gmail.com>
2020-03-22 16:45:30 +0530

    Refactored code.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,34 +1,13 @@
 import React from 'react'
-import { Container, Navbar, Nav, Button, Row, Col } from 'react-bootstrap'
-import logo from './Assets/logo.svg'
-import Leaderboard from './Assets/leaderboard.svg'
-import CampCoin from './Assets/campcoins.svg'
-import BannerImg from './Assets/header-photo.png'
-import { NavRight, BannerWrapper, PaddingWrapper } from './Assets/style'
+import { Container, Row, Col } from 'react-bootstrap'
+import CampCoin from '../Assets/campcoins.svg'
+import BannerImg from '../Assets/header-photo.png'
+import { BannerWrapper, PaddingWrapper } from '../Assets/style'
 
 
 const Home = () => {
     return (
         <>
-        <Navbar expand="lg" className="mainMenu" fixed="top">
-            <Container>
-                <Navbar.Brand href="#home"><img src={logo} /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home" className="active">ONLINE COURSES</Nav.Link>
-                        <Nav.Link href="#features">OFFLINE CAMPS</Nav.Link>
-                        <Nav.Link href="#pricing">REFER & EARN</Nav.Link>
-                    </Nav>
-                    <NavRight>
-                        <a href=""><img src={Leaderboard}/></a>
-                        <Button variant="primary">FREE TRIAL</Button>
-                        <Button variant="primary">LOG IN</Button>
-                    </NavRight>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-      
         <BannerWrapper>
         <Container>
             <PaddingWrapper>
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Container, Navbar, Nav, Button } from 'react-bootstrap'
+import logo from '../Assets/logo.svg'
+import Leaderboard from '../Assets/leaderboard.svg'
+import { NavRight } from '../Assets/style'
 
-const Header = () => {
-    return (<div>Header</div>);
+const Header = props => {
+    return (
+        <Navbar expand="lg" className="mainMenu" fixed="top">
+            <Container>
+                <Navbar.Brand href="#home"><img src={logo} alt={'logo'} /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home" className="active">ONLINE COURSES</Nav.Link>
+                        <Nav.Link href="#features">OFFLINE CAMPS</Nav.Link>
+                        <Nav.Link href="#pricing">REFER & EARN</Nav.Link>
+                    </Nav>
+                    <NavRight>
+                        <a href=""><img src={Leaderboard} alt={'Leaderboard'} /></a>
+                        <Button variant="primary">FREE TRIAL</Button>
+                        <Button variant="primary">LOG IN</Button>
+                    </NavRight>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default Header;
