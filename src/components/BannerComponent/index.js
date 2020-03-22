36c6e8d13885fcc585a2bb0c3ef429113c450ce2
Pravin <isppal02@gmail.com>
2020-03-22 17:15:12 +0530

    Created banner component.

---
+++ src/components/BannerComponent/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap'
+import CampCoin from '../../Assets/campcoins.svg'
+import BannerImg from '../../Assets/header-photo.png'
+import { BannerWrapper, PaddingWrapper } from '../../Assets/style'
+
+const BannerComponent = () => {
+    return (
+        <BannerWrapper>
+            <Container>
+                <PaddingWrapper>
+                    <Row>
+                        <Col>
+                        <h1>Welcome to the school of the future.</h1>
+                        <p className="subhead">Cutting-edge technology courses for Ages 8 - 18. Unlock <span className="yellow"> 21st century skills</span>, earn <img src={CampCoin} />, and build a college-ready portfolio as you learn.</p>
+                        </Col>
+                        <Col>
+                            <img src={BannerImg}  alt="banner"/>
+                        </Col>
+                    </Row>
+                </PaddingWrapper>
+            </Container>
+        </BannerWrapper>
+    );
+}
+
+export default BannerComponent;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,29 +1,10 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import CampCoin from '../Assets/campcoins.svg'
-import BannerImg from '../Assets/header-photo.png'
-import { BannerWrapper, PaddingWrapper } from '../Assets/style'
+import BannerComponent from '../components/BannerComponent';
 
 
 const Home = () => {
     return (
-        <>
-        <BannerWrapper>
-        <Container>
-            <PaddingWrapper>
-                <Row>
-                    <Col>
-                      <h1>Welcome to the school of the future.</h1>
-                      <p className="subhead">Cutting-edge technology courses for Ages 8 - 18. Unlock <span className="yellow"> 21st century skills</span>, earn <img src={CampCoin} />, and build a college-ready portfolio as you learn.</p>
-                    </Col>
-                    <Col>
-                        <img src={BannerImg}  alt="banner"/>
-                    </Col>
-                </Row>
-            </PaddingWrapper>
-        </Container>
-        </BannerWrapper>
-      </>
+        <BannerComponent />
     );
 }
 
