e13131bb4fd51cb17e259bd8c19ebeaadcb1c112
Pravin <isppal02@gmail.com>
2020-03-22 17:23:58 +0530

    Added slick-carousel

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './layout/Header';
 import Footer from './layout/Footer';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './App.css';
 
 function App() {
