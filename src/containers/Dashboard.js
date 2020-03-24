6c0c440c9758f036236f065bcb34ce229e3bdc45
somil <somildhane@gmail.com>
2020-03-24 19:35:09 +0530

    banner UI changes

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap'
 import LearnIcon from '../Assets/learn.svg'
+import AnywhereIcon from '../Assets/anywhere.svg'
+import PioneerIcon from '../Assets/pioneer.svg'
+import DoingIcon from '../Assets/doing.svg'
 import { BannerWrapper, PaddingWrapper, BannerCard, Sqwrapper } from '../Assets/style'
 import TabsComponent from '../components/TabsComponent';
 const Dashboard = () => {
@@ -20,23 +23,23 @@ const Dashboard = () => {
                             </Col>
                             <Col expand="md">
                                 <BannerCard>
-                                    <img src={LearnIcon} alt="LearnIcon " />
-                                    <h3>Learn interactively.</h3>
-                                    <p>LIVE online sessions with our expert mentors.<a href=""> See a demo.</a></p>
+                                    <img src={AnywhereIcon} alt="LearnIcon " />
+                                    <h3>Learn from anywhere.</h3>
+                                    <p>Convenience and safety for you and your child.</p>
                                 </BannerCard>
                             </Col>
                             <Col expand="md">
                                 <BannerCard>
-                                    <img src={LearnIcon} alt="LearnIcon " />
-                                    <h3>Learn interactively.</h3>
-                                    <p>LIVE online sessions with our expert mentors.<a href=""> See a demo.</a></p>
+                                    <img src={PioneerIcon} alt="LearnIcon " />
+                                    <h3>Learn from the pioneers.</h3>
+                                    <p>We’ve been teaching kids to code since 2010.<a href=""> Read more.</a></p>
                                 </BannerCard>
                             </Col>
                             <Col expand="md">
                                 <BannerCard>
-                                    <img src={LearnIcon} alt="LearnIcon " />
-                                    <h3>Learn interactively.</h3>
-                                    <p>LIVE online sessions with our expert mentors.<a href=""> See a demo.</a></p>
+                                    <img src={DoingIcon} alt="LearnIcon " />
+                                    <h3>Learn by doing.</h3>
+                                    <p>100% project-based curriculum. Solve real-world problems. </p>
                                 </BannerCard>
                             </Col>
                         </Row>
