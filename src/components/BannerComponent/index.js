a0fbc46ccc2c9bc655717d07330d0c509390112b
somil <somildhane@gmail.com>
2020-03-25 23:20:21 +0530

    course page ui

--- src/components/BannerComponent/index.js
+++ src/components/BannerComponent/index.js
@@ -10,13 +10,14 @@ const BannerComponent = () => {
             <Container>
                 <PaddingWrapper>
                     <Row>
+                        <Col expand="md" className="order1">
+                            <img src={BannerImg}  alt="banner"/>
+                        </Col>  
                         <Col expand="md" className="order2">
                         <h1>Welcome to the school of the future.</h1>
                         <p className="subhead">Cutting-edge technology courses for Ages 8 - 18. Unlock <span className="yellow"> 21st century skills</span>, earn <img alt={'CampCoin'} src={CampCoin} />, and build a college-ready portfolio as you learn.</p>
                         </Col>
-                        <Col expand="md" className="order1">
-                            <img src={BannerImg}  alt="banner"/>
-                        </Col>
+                      
                     </Row>
                 </PaddingWrapper>
             </Container>
